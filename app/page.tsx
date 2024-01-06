6dae21ce2a73766e3b8582ceae6b16708079447a
kourosh-H <kourosh.hajivand0@gmail.com>
2024-01-06 11:23:15 +0330

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center uppercase text-4xl bg-black font-black text-white ">
-      LETS Begin Update Test
+      LETS Begin Update Test New test on local
     </div>
   );
 }
