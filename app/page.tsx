cb372ce79977ee10e6293b65e7b3044973b01655
kourosh-H <kourosh.hajivand0@gmail.com>
2024-01-06 11:01:19 +0330

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center uppercase text-4xl bg-black font-black ">
+    <div className="w-full min-h-screen flex items-center justify-center uppercase text-4xl bg-black font-black text-white ">
       LETS Begin
     </div>
   );
