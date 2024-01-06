d68859b51277265faa65f177115275dcb73cea97
kourosh-H <kourosh.hajivand0@gmail.com>
2024-01-06 11:49:52 +0330

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center uppercase text-4xl bg-black font-black text-white ">
-      Develop Branch
+      Develop Branch Test
     </div>
   );
 }
