b270ea4c1635adb70ee59267e21a1958ec7694cd
Shayan Rezvani <shayanrezvani@Shayans-MacBook-Pro.local>
2024-01-06 12:30:20 +0330

    add

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center uppercase text-4xl bg-black font-black text-white ">
       LETS Begin
+      Test
     </div>
   );
 }
