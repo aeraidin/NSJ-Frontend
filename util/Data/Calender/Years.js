99f3766cda1b9e7574c5c9d28c6375709fc74fbc
kourosh-H <kourosh.hajivand0@gmail.com>
2024-01-08 18:52:08 +0330

    🐛 bug

--- util/Data/Calender/Years.js
+++ util/Data/Calender/Years.js
@@ -1,10 +1,3 @@
-interface YearOption {
-  label: string;
-  value: string;
-}
-
-const Years: YearOption[] = [];
-
 for (let i = 1300; i <= 1499; i++) {
   Years.push({ label: i.toString(), value: i.toString() });
 }
