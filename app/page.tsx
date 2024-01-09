aae133f56d26d2931fc4862b6f139b2f3cc36733
Shayan Rezvani <shayanrezvani@Shayans-MacBook-Pro.local>
2024-01-09 12:01:29 +0330

    gender update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
+"use client"
 import PrimaryBtn from "@/components/Layout/Buttons/PrimaryBtn";
 import Gender from "@/components/Layout/Forms/Gender";
-import LoginForm from "@/components/Layout/Forms/auth/LoginForm";
 import FullLayout from "@/components/Layout/FullLayout";
-import Otpcode from "@/components/Layout/Otpcode/Otpcode";
 import Register from "@/components/Modals/RegisterModal/Register";
 import Link from "next/link";
 
@@ -11,15 +10,11 @@ export default function Home() {
   return (
     <FullLayout>
       <div className="w-full min-h-screen flex items-center justify-center">
-        {/* <div className="w-full max-w-[150px]">
+        <div className="w-full max-w-[150px]">
           <Link href={"/login"}>
             <PrimaryBtn>ورود</PrimaryBtn>
           </Link>
-        </div> */}
-
-
-        {/* <Register ></Register> */}
-        <Gender />
+        </div>
       </div>
     </FullLayout>
   );
--- components/Layout/Buttons/RadioButton.tsx
+++ components/Layout/Buttons/RadioButton.tsx
@@ -6,9 +6,10 @@ interface RadioBtnProps {
   checked: boolean;
   disabled: boolean;
   error: boolean;
+  label:string;
 }
 
-function RadioButton({ onChange, checked, disabled, error }: RadioBtnProps) {
+function RadioButton({ onChange, checked, disabled, error, label }: RadioBtnProps) {
   const toggleHandler = () => {
     if (!disabled) {
       onChange(!checked);
@@ -20,14 +21,20 @@ function RadioButton({ onChange, checked, disabled, error }: RadioBtnProps) {
 
   return (
     <div
-      onClick={toggleHandler}
+    onClick={toggleHandler}
+    className=" flex cursor-pointer group "
+    >
+        <label className="text-base px-3 group-hover:cursor-pointer select-none">{label}</label>
+
+    <div
       className={`${
         disabled ? "cursor-not-allowed " : "cursor-pointer"
-      } ${error ? " bg-red-300 border border-red-500" : null} rounded-full w-5 h-5 flex justify-center items-center bg-white border border-gray-100`}
+      } ${error ? " bg-red-300 border border-red-500" : null}  rounded-full w-5 h-5 flex justify-center items-center bg-white border border-gray-100`}
     >
+
       {checked ? (
         <div
-          className={`w-3 h-3 ${
+          className={`w-3 h-3  ${
             error ? "bg-red-500" : "bg-black"
           }  rounded-full `}
         ></div>
@@ -35,6 +42,8 @@ function RadioButton({ onChange, checked, disabled, error }: RadioBtnProps) {
       {disabled ? (
         <div className=" w-3 h-3 bg-gray-300 rounded-full"></div>
       ) : null}
+    </div>
+    
     </div>
   );
 }
