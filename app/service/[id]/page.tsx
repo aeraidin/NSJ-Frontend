0a23e7df0d737a424a74accc1892e9b70423fd9b
ShayanRezvani <shayanrezvani5@gmail.com>
2024-05-01 15:46:13 +0330

    💄 Add Some UI Service Page !

--- app/service/[id]/page.tsx
+++ app/service/[id]/page.tsx
@@ -1,32 +1,31 @@
-import AboutService from '@/components/page/SingleService/AboutService'
-import ConditionsService from '@/components/page/SingleService/ConditionsService'
-import ContactService from '@/components/page/SingleService/ContactService'
-import MainServiceInfo from '@/components/page/SingleService/MainServiceInfo'
-import RelatedService from '@/components/page/SingleService/RelatedService'
-import ReviewService from '@/components/page/SingleService/ReviewService'
-import SansService from '@/components/page/SingleService/SansService'
-import React from 'react'
+/** @format */
 
-function page({
-    params,
-}: {
-    params: { id: string };
-}) {
-    console.log('====================================');
-    console.log(params.id);
-    console.log('====================================');
-    return (
-        <div className=' flex flex-col w-full'>
+import AboutService from "@/components/page/SingleService/AboutService";
+import ConditionsService from "@/components/page/SingleService/ConditionsService";
+import ContactService from "@/components/page/SingleService/ContactService";
+import MainServiceInfo from "@/components/page/SingleService/MainServiceInfo";
+import RelatedService from "@/components/page/SingleService/RelatedService";
+import ReviewService from "@/components/page/SingleService/ReviewService";
+import SansService from "@/components/page/SingleService/SansService";
+import useGetSingleService from "@/util/hook/SingleService/useGetSingleService";
+import React from "react";
 
-            <MainServiceInfo />
-            <SansService />
-            <AboutService />
-            <ContactService />
-            <ConditionsService />
-            <ReviewService />
-            <RelatedService />
-        </div>
-    )
+function Page({ params }: { params: { id: string } }) {
+  console.log("====================================");
+  console.log(params.id);
+  console.log("====================================");
+
+  return (
+    <div className=" flex flex-col w-full Container divide-y divide-gray-50 ">
+      <MainServiceInfo />
+      <SansService />
+      <AboutService param={params.id} />
+      <ContactService param={params.id} />
+      <ConditionsService param={params.id} />
+      <ReviewService />
+      <RelatedService />
+    </div>
+  );
 }
 
-export default page
+export default Page;
--- components/page/SingleService/AboutService.tsx
+++ components/page/SingleService/AboutService.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+/** @format */
+"use client";
+import React, { useState } from "react";
+import { Collapse } from "react-collapse";
 
-function AboutService() {
-  return (
-    <div>AboutService</div>
-  )
+interface AboutServiceProps {
+  param: string;
 }
 
-export default AboutService
+function AboutService({ param }: AboutServiceProps) {
+  const [expanded, setExpanded] = useState(false);
+  // const about = useGetSingleService({ id: param });
+
+  const para = `        چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
+            برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود
+            ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال
+            و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها
+            شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و
+            فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت
+            که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان
+            رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات
+            پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.`;
+  return (
+    <div className="flex gap-y-6 flex-col py-10">
+      {/* <h2>{`معرفی ${about.data?.value?.service.name}`}</h2> */}
+      <div className="w-full  overflow-hidden">
+        {/* <Collapse isOpened={expanded}>
+          <p></p>
+        </Collapse> */}
+
+        <p className=" ">{expanded ? para : `${para.substring(0, 550)}... `}</p>
+
+        <button
+          className="text-third-600  mt-6 cursor-pointer hover:text-third-400 duration-200"
+          onClick={() => {
+            setExpanded(!expanded);
+          }}
+        >
+          {expanded ? "توضیحات کمتر" : "توضیحات بیشتر"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AboutService;
