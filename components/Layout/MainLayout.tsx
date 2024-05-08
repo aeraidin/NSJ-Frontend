427b8835daea1247067570d560ab08071fad9d5c
kourosh-H <kourosh.hajivand0@gmail.com>
2024-05-08 21:44:26 +0330

    🚀 Add Some ui Update

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header/Header'
 import Footer from './Footer'
+import { useSestion } from '@/util/session';
 
 function MainLayout({ children }: { children: React.ReactNode }) {
+    const Session = useSestion();
+
     return (
         <React.Fragment>
-
-            <Header />
-
+            <Header isLoggedin={Session} />
             <main className="min-h-screen">{children}</main>
             <Footer />
         </React.Fragment>
