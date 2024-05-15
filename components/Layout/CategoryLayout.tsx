88ff817a7ce20e05988c429a5129f2b521357633
kourosh-H <kourosh.hajivand0@gmail.com>
2024-05-15 21:06:24 +0330

    update

--- components/Layout/CategoryLayout.tsx
+++ components/Layout/CategoryLayout.tsx
@@ -6,10 +6,18 @@ import { useSearch } from '@/util/api/Search/useSearch';
 
 function CategoryLayout({ serviceName, serviceId, Insearch }: { serviceName?: string, serviceId?: number, Insearch?: boolean }) {
     const [Data, setData] = useState(null)
+    const [MinPrice, setMinPrice] = useState(0)
+    const [MaxPrice, setMaxPrice] = useState(0)
+    const [MinRate, setMinRate] = useState(0)
+    const [Sortby, setSortby] = useState(0)
+
     const searchHandler = useMutation({
         mutationFn: useSearch,
         onSuccess(data, variables, context) {
+            console.log("SoOOOSSOSOSO");
             console.log(data);
+            console.log("SoOOOSSOSOSO");
+
         },
         onError(error, variables, context) { },
     });
@@ -18,7 +26,10 @@ function CategoryLayout({ serviceName, serviceId, Insearch }: { serviceName?: st
             page: 1, pageSize: 20,
             serviceId: serviceId,
             serviceName: serviceName,
-            sortTyp: 0
+            sortTyp: Sortby,
+            maxPrice: MaxPrice,
+            minPrice: MinPrice,
+            minRate: MinRate,
         })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -31,12 +42,18 @@ function CategoryLayout({ serviceName, serviceId, Insearch }: { serviceName?: st
                 <div className='w-full flex gap-6'>
                     {/* Fillter Part */}
                     <div className='hidden lg:block w-full max-w-[316px] sticky top-28 h-[200px] bg-red-400'>
-                        s
+
                     </div>
                     {/* Body */}
                     <div className='w-full flex flex-1 flex-col gap-6'>
                         {/* Sort */}
-                        <div className='w-full h-[100px] bg-blue-400'>
+                        <div className='w-full px-6 pt-4 rounded-2xl border border-gray-50 flex items-center gap-4'>
+                            <p className='pb-4'>مرتب سازی بر اساس:</p>
+                            <button onClick={() => setSortby(0)} className={`TabBtn ${Sortby === 0 ? "TabBtnActice" : ""}`}>پیش فرض</button>
+                            <button onClick={() => setSortby(1)} className={`TabBtn ${Sortby === 1 ? "TabBtnActice" : ""}`} >جدید ترین</button>
+                            <button onClick={() => setSortby(2)} className={`TabBtn ${Sortby === 2 ? "TabBtnActice" : ""}`}>محبوب ترین</button>
+                            {/* <button className='TabBtn'>بیشترین تخفیف</button> */}
+
                         </div>
                         {/* Items */}
                         <div className='w-full grid lg:grid-cols-3 h-[1000px] bg-orange-400'>
