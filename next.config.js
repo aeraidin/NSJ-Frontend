b8af539ba5859e14085fc152e061f340efacfba1
kourosh-H <kourosh.hajivand0@gmail.com>
2024-05-27 08:24:37 +0330

    🐛 🔥 debug

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
  experimental: {
   missingSuspenseWithCSRBailout: false,
  },
+ webpack: (config, { isServer }) => {
+  config.resolve.alias["@"] = path.resolve(__dirname);
+  return config;
+ },
  images: {
   remotePatterns: [
    {
