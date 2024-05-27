ed594ca8890a91ccba5925deff1a6d435d7fdbd2
ShayanRezvani <shayanrezvani5@gmail.com>
2024-05-27 12:25:12 +0330

    🐛 fix check token profile !

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,11 +1,22 @@
+/** @format */
+"use client";
 import MainLayout from "@/components/Layout/MainLayout";
 import SideBar from "@/components/page/Profile/SideBar";
-import React from "react";
-function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { redirect, usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+import Cookies from "js-cookie";
+import Breadcrumb from "@/components/Layout/breadcrumb";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const path = usePathname();
+
+  const token = Cookies.get("token");
+  useEffect(() => {
+    if (!token) {
+      redirect("/login");
+    }
+  }, [token]);
   return (
     <MainLayout>
       <div className=" w-full  lg:gap-x-8 my-8   flex">
@@ -16,7 +27,7 @@ function Layout({
         </div>
         <div className="flex-1 lg:flex flex-col">
           <div className=" hidden lg:block">
-            {/* <Breadcrumb>
+            <Breadcrumb>
               <Breadcrumb.Item href="/">خانه</Breadcrumb.Item>
               <Breadcrumb.Item href={path}>
                 {path === "/profile/info" ? "اطلاعات حساب کاربری" : null}
@@ -26,7 +37,7 @@ function Layout({
                 {path === "/profile/transactions" ? "تراکنش ها" : null}
                 {path === "/profile/comments" ? " نظرات من" : null}
               </Breadcrumb.Item>
-            </Breadcrumb> */}
+            </Breadcrumb>
           </div>
           <div className="lg:flex-1 overflow-y-auto fixed lg:static top-7 left-0 right-0 bottom-0 bg-white lg:flex lg:px-[146px] mt-[70px]">
             {children}
