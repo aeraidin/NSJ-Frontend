dcbe035ba185703788a447cc97b6ee83ba043196
ShayanRezvani <shayanrezvani5@gmail.com>
2024-05-30 20:10:22 +0330

    ✅ Added Update Profile !

---
+++ util/convertFileToBase64.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+export const convertFileToBase64 = (file: File): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64String = reader.result?.toString().split(",")[1];
+      if (base64String) {
+        resolve(base64String);
+      } else {
+        reject(new Error("Failed to convert file to base64."));
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+};
