1b44d42c3a304d0c6e212576c6bdb7439e988cb3
kourosh-H <kourosh.hajivand0@gmail.com>
2024-05-31 12:32:06 +0330

    🔒️ Protect Route

--- util/session.ts
+++ util/session.ts
@@ -1,8 +1,9 @@
-"use server";
 import { cookies } from "next/headers";
 
 export const useSestion = () => {
  const cookieStore = cookies();
  const isLoggedin = cookieStore.get("token");
+ console.log(Boolean(isLoggedin?.value));
+
  return Boolean(isLoggedin?.value);
 };
